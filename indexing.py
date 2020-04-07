6d3d802c924f6333b6cd59047f8c26a5b0632850
ssephillip <phillip.sittigweb.de>
2020-04-07 21:12:00 +0200

    adds description of the endpoints and removes commented out code

--- indexing.py
+++ indexing.py
@@ -6,7 +6,57 @@ from pathlib import Path
 
 app = Flask(__name__)
 
-
+#This service provides the possibility to store vectors in a vector index and to retrieve the k-Nearest-Neighbours, given the ID of a vector in the Index.
+#This functionality can be accessed via Http Requests.
+#
+#The following endpoints are provided by this service:
+#
+#Indexing vectors:
+#Endpoint:
+#[baseurl]/index_vectors
+#
+#Example:
+#http://localhost:5000/index_vectors
+#
+#Type: POST-Request
+#
+#Provides the possibility to store vectors in the vector index.
+#The existing vectors in the index will be removed.
+#As parameters two files have to be provided.
+#A file containing the ids of the vectors and a file containing the vectors.
+#Both files are passed as key-value pairs.
+#The key is "ids" for the file containing the IDs and "vectors" for the file containing the vectors.
+#The values are the files theselves. It is recommended to send the request as a Http Multipart Request.
+#When using this service with newsleak, the newsleak document IDs should be used as IDs. 
+#Thereby, vectors can easily be associated with the corresponding document.
+#
+#The files need to look as follows:
+#In each file, each line must only contain one ID/vector.
+#Also the ID and the corresponding vector must have the same line number.
+#Example: 
+#The ID in line 5 in the files with the IDs must be the ID of the vector in line 5 in the file containing the vectors.
+#
+#
+#
+#Retrieve the k-Nearest-Neighbours:
+#Endpoint:
+#[baseurl]/vectors/<ID>?num=[num of k-NN]
+#
+#Example:
+#http://localhost:5000/vector/7?num=10
+#
+#Type: GET-Request
+#
+#Retrieves the k-Nearest-Neighbours of the vector for which an ID was provided.
+#Note: The vector belonging to the specified ID is also returned as result (as the most similar vector).
+#The result is returned in JSON format. It is returned as a list, where each list element is a pair of the following form:
+#[vector-id,cosine_distance]
+#
+#Example list item:
+#[5,0.32432] 
+#
+#Example response:
+#{"result":[[3,0.0],[15,0.013967633247375488],[16,0.017206192016601562],[13,0.018164396286010742]]}
 
 
 class Indexer:
@@ -18,7 +68,6 @@ class Indexer:
 
 
     def __init__(self):
-        print("juhuu")
         index_file = Path(self.index_file_name)
         id_file = Path(self.id_file_name)
         vector_file = Path(self.data_file_name)
@@ -34,7 +83,7 @@ class Indexer:
     def example(self):
         return 'Server Works!'
 
-    #@app.route('/index_vectors', methods=['POST'])
+    
     def index_vectors(self):
 
         vector_file = request.files['vectors']
@@ -61,7 +110,7 @@ class Indexer:
         return "OK"
 
 
-    #@app.route('/vector/<int:id>')
+    
     def get_vector(self, id):
         num_of_NN = int(request.args.get('num'))
         print(num_of_NN)
